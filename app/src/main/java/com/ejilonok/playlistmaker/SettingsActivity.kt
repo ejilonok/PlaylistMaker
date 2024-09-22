a91fdf181406d1f981900e0f30695b955d9eb00d
ejilonok <awesome.ejilonok@yandex.ru>
2024-09-22 11:13:00 +0300

    В стили добавлены шрифты, продолжено внедрение стилей, пункты меню настроек переделаны в кнопки, проведена ревизия цветов для внедрения тем, добавлена навигационная панель в настройки, убрана кнопка возврата в настройках, проверено вынесение строк в strings, из dimens.xml убраны неиспользуемые размеры

--- app/src/main/java/com/ejilonok/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/ejilonok/playlistmaker/SettingsActivity.kt
@@ -1,16 +1,10 @@
 package com.ejilonok.playlistmaker
 
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-
-        val backward = findViewById<ImageView>(R.id.backward)
-        backward.setOnClickListener {
-            finish()
-        }
     }
 }
