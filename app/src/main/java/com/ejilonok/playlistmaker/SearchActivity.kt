220171814e2c7ec0d6df19e12f0e319ae820579d
ejilonok <awesome.ejilonok@yandex.ru>
2024-10-10 15:37:14 +0300

    Добавлен TextWatcher для поля поиска и реализована логика работы кнопки стирания

--- app/src/main/java/com/ejilonok/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/ejilonok/playlistmaker/SearchActivity.kt
@@ -2,10 +2,44 @@ package com.ejilonok.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View.*
+import android.view.WindowManager
+import android.widget.EditText
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val searchLine = findViewById<EditText>(R.id.search_line)
+        val clearButton = findViewById<ImageView>(R.id.clear_button)
+
+        clearButton.setOnClickListener() {
+            searchLine.setText("")
+            window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN)
+        }
+
+        val clearTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.isNullOrEmpty()) {
+                    clearButton.visibility = GONE
+                } else
+                    clearButton.visibility = VISIBLE
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+
+        searchLine.addTextChangedListener(clearTextWatcher)
+
     }
 }
