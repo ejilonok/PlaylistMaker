c2dcaad8ac58aa53860aa9a02733bff00331c7ca
ejilonok <awesome.ejilonok@yandex.ru>
2024-10-14 14:23:35 +0300

    Настроен переключатель тем. Сделано сохранение переключателя тем через sharedPreferences.
    Обнаружена поломка других кнопок настроек

--- app/src/main/java/com/ejilonok/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/ejilonok/playlistmaker/SettingsActivity.kt
@@ -1,15 +1,26 @@
 package com.ejilonok.playlistmaker
 
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class SettingsActivity : AppCompatActivity() {
+    private var themeSwitch : SwitchMaterial? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        themeSwitch = findViewById<SwitchMaterial>(R.id.theme_switch)
+        themeSwitch!!.isChecked = getSavedThemeState()
+        themeSwitch!!.setOnCheckedChangeListener() { _, isChecked ->
+            setDarkTheme(isChecked)
+            saveThemeState(isChecked)
+        }
 
         val shareButton = findViewById<MaterialButton>(R.id.share_app)
         shareButton.setOnClickListener() {
@@ -35,5 +46,33 @@ class SettingsActivity : AppCompatActivity() {
             termsIntent.data = Uri.parse(getString(R.string.terms_link))
             startActivity(termsIntent)
         }
+    }
+
+    private fun setLightTheme() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+
+    private fun setDarkTheme() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+    }
+
+    private fun setDarkTheme(isDark : Boolean) {
+        if (isDark)
+            setDarkTheme()
+        else
+            setLightTheme()
+    }
+
+    private fun getSavedThemeState() : Boolean {
+        val sharedPreferences = getSharedPreferences("ThemeSettings", Context.MODE_PRIVATE)
+        val systemTheme = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
+        return sharedPreferences.getBoolean("isDarkTheme", systemTheme)
+    }
+
+    private fun saveThemeState(isDark: Boolean) {
+        val sharedPreferences = getSharedPreferences("ThemeSettings", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("isDarkTheme", isDark)
+        editor.apply()
     }
 }
